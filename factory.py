50226c42c479a81731d1e6c2b69acfd59c7518cb
Ravi Kuril <ravikuril13@gmail.com>
2020-09-13 03:14:33 +0530

    Update factory.py

--- factory.py
+++ factory.py
@@ -28,3 +28,21 @@ class language_selector:
 print(language_selector().factory("language1").get_list())
 print(language_selector().factory("language2").get_list())
 print(language_selector().factory("language3").get_list())
+
+
+
+"""
+Advantages of using Factory method:
+
+    We can easily add the new types of products without disturbing the existing client code.
+    Generally, tight coupling is being avoided between the products and the creator classes and objects.
+
+Disadvantages of using Factory method:
+
+    To create a particluar concrete product object, client might have to sub-class the creator class.
+    You end up with huge number of small files i.e, cluttering of the files.
+    Applicability :
+        In a Graphics system, depending upon the user’s input it can draw different shapes like Rectangle, Square, Circle, etc. But for the ease of both developers as well as the client, we can use the factory method to create the instance depending upon the user’s input. Then we don’t have to change the client code for adding a new shape.
+        In a Hotel booking site, we can book a slot for 1 room, 2 rooms, 3 rooms, etc. Here user can input the number of rooms he wants to book. Using the factory method, we can create a factory class AnyRooms which will help us to create the instance depending upon the user’s input. Again we don’t have to change the client’s code for adding the new facility.
+
+"""
